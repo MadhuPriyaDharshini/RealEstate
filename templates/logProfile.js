75b0c70615322bf63b178db415bd93c8e5754f76
MadhuPriyaDharshini <madhupriya@student.tce.edu>
2023-02-13 22:42:58 +0530

    modified:   .hintrc 	modified:   templates/carousel.css 	modified:   templates/logProfile.css 	modified:   templates/logProfile.html 	modified:   templates/logProfile.js 	modified:   templates/login.html

---
+++ templates/logProfile.js
@@ -0,0 +1,28 @@
+function phonenumber(inputtxt) {
+    console.log(inputDate);
+    var phoneno = /^\d{10}$/;
+  
+    if (inputtxt.value.match(phoneno)) {
+      return true;
+    } else {
+      alert("Not a valid Phone Number");
+      return false;
+    }
+  }
+  
+  document.getElementById("date1").addEventListener("change", function(){
+    var date = this.value;
+    var enteredDate = new Date(date);
+    console.log(enteredDate);
+    var startDate = new Date("1992-01-01");
+    var endDate = new Date("2002-03-04");
+    if (enteredDate < startDate || enteredDate > endDate) {
+      alert("Enter valid Date");
+      this.value = ""
+    }
+  });
+  
+  
+  document.getElementById("password").addEventListener("input",function(){
+     
+  })
