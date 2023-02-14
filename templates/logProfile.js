dbc3c7cd2f4151f0a0253f3ad4cdbebf90892da4
Lokesh <lokeshtrt0108@gmail.com>
2023-02-14 15:33:28 +0530

    modified:   templates/logProfile.html 	modified:   templates/logProfile.js

--- templates/logProfile.js
+++ templates/logProfile.js
@@ -1,40 +1,36 @@
-function phonenumber(inputtxt) {
-  console.log(inputDate);
-  var phoneno = /^\d{10}$/;
+// function phonenumber(inputtxt) {
+//   console.log(inputDate);
+//   var phoneno = /^\d{10}$/;
 
-  if (inputtxt.value.match(phoneno)) {
-    return true;
-  } else {
-    alert("Not a valid Phone Number");
-    return false;
-  }
-}
+//   if (inputtxt.value.match(phoneno)) {
+//     return true;
+//   } else {
+//     alert("Not a valid Phone Number");
+//     return false;
+//   }
+// }
 
-const validDate = document.getElementById("dateElement")
+// // document.querySelector('#dataText').addEventListener("change", function () {
+// //   var date = this.value;
+// //   var enteredDate = new Date(date);
+// //   console.log(enteredDate);
+// //   var startDate = new Date("1992-01-01");
+// //   var endDate = new Date("2022-03-04");
+// //   if (enteredDate < startDate || enteredDate > endDate) {
+// //     alert("Enter valid Date");
+// //     this.value = "";
+// //   }
+// // });
 
-console.log(validDate)
+// const form = document.getElementById("formElement");
+// const submit = document.getElementById("submitButton");
 
-validDate.addEventListener("change", function () {
-  var date = this.value;
-  var enteredDate = new Date(date);
-  console.log(enteredDate);
-  var startDate = new Date("1992-01-01");
-  var endDate = new Date("2022-03-04");
-  if (enteredDate < startDate || enteredDate > endDate) {
-    alert("Enter valid Date");
-    this.value = "";
-  }
-});
+// form.addEventListener("keydown", function (event) {
+//   if (event.key === "Enter") {
+//     submit.click();
+//   }
+// });
 
-const form = document.getElementById("formElement");
-const submit = document.getElementById("submitButton");
-
-form.addEventListener("keydown", function (event) {
-  if (event.key === "Enter") {
-    submit.click();
-  }
-});
-
-function validateForm() {
-  alert("Submitted Successfully");
-}
+// function validateForm() {
+//   alert("Submitted Successfully");
+// }
